7d4af469c8b84adb8e9cc6344783b82d9c8eee3a
lilveth <lizsbeth5@gmail.com>
2024-12-30 15:50:21 -0300

    Proyecto deploy

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { useReducer, useEffect, useMemo  } from 'react';
+import Form from "./components/Form"
+import { activityReducer, initialState } from './reducers/activity-reducer'
+import ActivityList from './components/ActivityList'
+import CalorieTracker from './components/CalorieTracker';
+function App() {
+ 
+ const [state, dispatch] = useReducer ( activityReducer, initialState)
+
+useEffect(()=> {
+  localStorage.setItem('activities', JSON.stringify(state.activities))
+}, [state.activities])
+
+const canRestartApp = () => useMemo (() => state.activities.length , [state.activities])
+
+
+  return (
+    <>
+    <header className="bg-blue-950 py-3">
+      <div className="max-w-4xl mx-auto flex justify-between">
+      <h1 className="text-center text-lg font-bold text-white upercase">
+      Contador de Calorias
+     </h1>
+
+     <button
+     className='bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white
+     cursor-pointer rounded-lg text-sm disabled:opacity-10'
+     disabled={!canRestartApp()}
+     onClick={()=>dispatch({type:'restart-app'})}
+     >
+      Reiniciar App
+     </button>
+      </div>
+     
+
+    </header>
+<section className="bg-blue-400 py-20 px-5">
+  <div className="max-w-4xl mx-auto">
+   <Form 
+    dispatch={dispatch}
+    state={state}
+   />
+  </div>
+</section>
+<section className='bg-gray-800 py-10'>
+ <div className='max-w-4xl mx-auto'>
+ <CalorieTracker
+  activities={state.activities}
+ />
+ </div>
+</section>
+
+<section className='p-10 mx-auto max-w-4xl'>
+  <ActivityList
+     activities={state.activities}
+     dispatch={dispatch}
+  />
+  
+</section>
+
+
+    </>
+  )
+}
+
+export default App
